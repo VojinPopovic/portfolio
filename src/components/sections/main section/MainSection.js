9a68ff322cc8cce8ec90470d1d8389a6130cc432
VojinPopovic <vojinpopovic3@gmail.com>
2022-11-24 18:53:10 +0100

    main section finished

--- src/components/sections/main section/MainSection.js
+++ src/components/sections/main section/MainSection.js
@@ -1,6 +1,7 @@
 import { MainSectionDiv } from "../../../styles/MainSection.style";
 import { CardStyle } from "../../../styles/Card.style";
 import cardImage from "./cardImage.png";
+import BlackBlobVojin from "../../../assets/BlackBlobVojin.svg";
 
 function MainSection() {
   return (
@@ -9,9 +10,45 @@ function MainSection() {
         <CardStyle>
           <div className="image-container">
             <img src={cardImage} alt="" />
+            <div className="me-container">
+              <p className="me-paragraph">Hi, i'm</p>
+              <span> Vojin</span>
+              <img src={BlackBlobVojin} alt="" />
+            </div>
+          </div>
+          <div className="card-text-container">
+            <p className="card-title">
+              A<span> frontend </span>
+              <br />
+              developer
+            </p>
+            <p className="paragraph">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio
+              ducimus explicabo atque quas sapiente? Laboriosam, vel quisquam
+              iure vero laudantium totam? Reprehenderit explicabo ad veniam ab
+              ipsam ea magnam porro temporibus aliquam impedit, autem, molestiae
+              ex exercitationem? Nesciunt, vitae porro.
+            </p>
           </div>
         </CardStyle>
-        <CardStyle mTop="40px"></CardStyle>
+        <CardStyle mTop="40px">
+          <div className="card-text-container">
+            <p className="card-title">
+              Always interested in
+              <br />
+              learning
+              <span> new </span>
+              things
+            </p>
+            <p className="paragraph">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio
+              ducimus explicabo atque quas sapiente? Laboriosam, vel quisquam
+              iure vero laudantium totam? Reprehenderit explicabo ad veniam ab
+              ipsam ea magnam porro temporibus aliquam impedit, autem, molestiae
+              ex exercitationem? Nesciunt, vitae porro.
+            </p>
+          </div>
+        </CardStyle>
       </div>
     </MainSectionDiv>
   );
--- src/styles/Card.style.js
+++ src/styles/Card.style.js
@@ -10,11 +10,32 @@ const CardStyle = styled.div`
   transform-style: preserve-3d;
   display: flex;
   justify-content: center;
-  & img{
+  align-items: center;
+  & img {
     width: 75%;
   }
   & p {
     color: white;
+  }
+  & .card-text-container {
+    width: 90%;
+    height: 80%;
+  }
+  & .card-title {
+    font-size: 2rem;
+    margin-bottom: 10px;
+    & span {
+      color: #da3636;
+      font-weight: 800;
+      letter-spacing: -0.05em;
+    }
+    letter-spacing: -0.05em;
+    line-height: 30px;
+  }
+  & .paragraph {
+    font-size: clamp(0.8rem, 2vw, 1rem);
+    font-weight: 300;
+    letter-spacing: 0em;
   }
   & .image-container {
     width: auto;
@@ -23,7 +44,29 @@ const CardStyle = styled.div`
     justify-content: center;
     position: absolute;
     top: -140%;
-    transform: translateZ(-10px)
+    transform: translateZ(-10px);
+  }
+  & .me-container {
+    width: auto;
+    position: absolute;
+    top: -63%;
+    font-size: clamp(3rem, 4vw, 5rem);
+    font-weight: 800;
+    letter-spacing: -0.05em;
+    display: flex;
+    & .me-paragraph {
+      color: black;
+      margin-right: 10px;
+    }
+    & span {
+      color: #da3636;
+    }
+    & img {
+      width: 60%;
+      position: absolute;
+      right: -55px;
+      z-index: -1;
+    }
   }
 `;
 
